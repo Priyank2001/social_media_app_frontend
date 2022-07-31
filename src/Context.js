f4c097789b48a05c4beec76eeee2fbc0d0d7c70f
Priyank <kadianpriyank@gmail.com>
2022-07-31 10:08:20 +0530

    third commit to deploy+ changed the frontendUrl in Context.js  + removed some bugs

--- src/Context.js
+++ src/Context.js
@@ -4,7 +4,7 @@ export default function Context(){
     return {
         // url:"http://localhost:8000",
         backendURL:"https://wallstreet-backend.herokuapp.com",  
-        frontendURL:"http://localhost:3000",
+        frontendURL:"www.ola-group6-media-app.herokuapp.com",
         appLogoImageURI:"https://icon-library.com/images/instagram-512_62607.png"
     }
 }
