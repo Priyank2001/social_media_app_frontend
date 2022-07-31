5e8994d3a3265b846f3e75faebf0198cec97a776
Priyank <kadianpriyank@gmail.com>
2022-07-31 10:14:17 +0530

    remove bugs

--- src/components/DropDownMenu.js
+++ src/components/DropDownMenu.js
@@ -12,6 +12,7 @@ export default function DropDownMenu(props) {
   const handleClose = () => {
     setAnchorEl(null);
   };
+  const frontendURL = "";
 
   return (
     <div>
@@ -33,8 +34,8 @@ export default function DropDownMenu(props) {
           'aria-labelledby': 'basic-button',
         }}
       >
-        <MenuItem onClick={(e) => {handleClose(e); window.location.assign(`${Context().frontendURL}`) }}>Home</MenuItem>
-        <MenuItem onClick={(e) => {handleClose(e); window.location.assign(`${Context().frontendURL}/user/${props.username}`) }}>My account</MenuItem>
+        <MenuItem onClick={(e) => {handleClose(e); window.location.assign(`${frontendURL}`) }}>Home</MenuItem>
+        <MenuItem onClick={(e) => {handleClose(e); window.location.assign(`${frontendURL}/user/${props.username}`) }}>My account</MenuItem>
         <MenuItem onClick={(e) => {handleClose(e); window.localStorage.removeItem("isLoggedIn"); window.location.assign(`${Context().frontendURL}/`)}}>Logout</MenuItem>
       </Menu>
     </div>
